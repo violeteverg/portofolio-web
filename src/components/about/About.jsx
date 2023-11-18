c7276b6fdbe30d3428166219965979733e9501c9
violeteverg <you@example.com>
2023-11-18 17:37:43 +0700

    fix size img

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,21 +1,34 @@
+import React, { useRef } from "react";
 import "./about.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const About = () => {
-  return (
-    <div className="about">
-      <div className="image_container">
-        <img src="/download.jpg" alt="foto" />
-      </div>
+  const ref = useRef();
 
-      <div className="wrapper">
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div className="about" ref={ref}>
+      <motion.div style={{ y: yBg }} className="image_container">
+        <img src="/download.jpg" alt="foto" />
+      </motion.div>
+
+      <motion.div style={{ x: yText }} className="wrapper">
         <h1>About Me</h1>
         <p>
           A bachelor degree from Terbuka University. Turning Full-Stack
-          Developer skilled in HTML,CSS,Javascript,React.js and Node.js Actively
-          enrolled in a Full-Stack Web Developer bootcamp to enhance web
-          development capabilities. Detail oriented and eager for a full time
-          opportunity in Full-Stack Web Development. Passionate about combining
-          biology knowledge with tech skills to create innovative solutions.
+          Developer skilled in HTML, CSS, Javascript, React.js, and Node.js.
+          Actively enrolled in a Full-Stack Web Developer bootcamp to enhance
+          web development capabilities. Detail oriented and eager for a
+          full-time opportunity in Full-Stack Web Development. Passionate about
+          combining biology knowledge with tech skills to create innovative
+          solutions.
         </p>
 
         <div className="skills">
@@ -27,7 +40,7 @@ const About = () => {
           <img src="/ReactJsLogo.png" alt="" />
           <img src="/NodeJsLogo.png" alt="" />
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
